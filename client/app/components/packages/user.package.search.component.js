68928efb9f3b65268a4b7151daffc1b1e66ca705
cis <rakesh.s@cisinlabs.com>
2017-03-18 21:30:48 +0530

    calender work for package

--- client/app/components/packages/user.package.search.component.js
+++ client/app/components/packages/user.package.search.component.js
@@ -27,6 +27,11 @@ var UserPackageSearchComponent = (function () {
         this.preferedType = "";
         this.additionalInstruction = "";
         this.selDate = { year: 0, month: 0, day: 0 };
+        this.packageCalender = {};
+        this.packagePriceType = '';
+        this.packagePrice = '';
+        this.packageDay = '';
+        this.packageMeridian = '';
         this.myDatePickerOptions = {
             // other options...
             dateFormat: 'dd-mm-yyyy',
@@ -35,11 +40,11 @@ var UserPackageSearchComponent = (function () {
             disableUntil: { year: 0, month: 0, day: 0 }
         };
         var d = new Date();
-        this.selDate = {
-            year: d.getFullYear(),
-            month: d.getMonth() + 1,
-            day: d.getDate()
-        };
+        // this.selDate = {
+        //   year: d.getFullYear(),
+        //   month: d.getMonth() + 1,
+        //   day: d.getDate()
+        // };
         this.myDatePickerOptions.disableUntil = {
             year: d.getFullYear(),
             month: d.getMonth() + 1,
@@ -48,9 +53,6 @@ var UserPackageSearchComponent = (function () {
         var params = this.activatedRoute.snapshot.queryParams;
         var currentUserStr = localStorage.getItem('currentUser');
         var currentUser = JSON.parse(currentUserStr);
-        if (params.id) {
-            this.selectedPackage = params.id;
-        }
         if (params.zip) {
             this.zipcode = params.zip;
         }
@@ -58,17 +60,125 @@ var UserPackageSearchComponent = (function () {
             this.zipcode = currentUser.token.zipcode;
         }
         this.getPackageByZipcode();
+        if (params.id) {
+            this.selectedPackage = params.id;
+        }
+        if (params.priceType) {
+            this.packagePriceType = params.priceType;
+        }
     }
     UserPackageSearchComponent.prototype.onDateChanged = function (event) {
         // event properties are: event.date, event.jsdate, event.formatted and event.epoc
+        //console.log()
+    };
+    UserPackageSearchComponent.prototype.createEnabledDays = function (startEventNumber, lastEventNumber, eventYear, eventMonth) {
+        console.log(startEventNumber + "," + lastEventNumber + "," + eventYear + "," + eventMonth);
+        var enableDays = this.myDatePickerOptions.enableDays;
+        while (startEventNumber <= lastEventNumber) {
+            enableDays.push({ year: eventYear, month: eventMonth, day: startEventNumber });
+            startEventNumber = startEventNumber + 7;
+        }
+        //this.myDatePickerOptions.enableDays= [{ year:2017,  month:3,  day:1},{ year:2017,  month:3,  day:8}];
+    };
+    UserPackageSearchComponent.prototype.getWeekDayCount = function (weekDay) {
+        var weekDayNumber = 0;
+        switch (weekDay) {
+            case 'su':
+            case 'sun':
+                weekDayNumber = 1;
+                break;
+            case 'mo':
+            case 'mon':
+                weekDayNumber = 2;
+                break;
+            case 'tu':
+            case 'tue':
+                weekDayNumber = 3;
+                break;
+            case 'we':
+            case 'wed':
+                weekDayNumber = 4;
+                break;
+            case 'th':
+            case 'thur':
+                weekDayNumber = 5;
+                break;
+            case 'fr':
+            case 'fri':
+                weekDayNumber = 6;
+                break;
+            case 'sa':
+            case 'sat':
+                weekDayNumber = 7;
+                break;
+        }
+        return weekDayNumber - 1;
+    };
+    UserPackageSearchComponent.prototype.onCalendarViewChanged = function (event) {
+        console.log('onCalendarViewChanged(): Year: ', event.year, ' - month: ', event.month, ' - first: ', event.first, ' - last: ', event.last);
+        var dayDiff = this.getWeekDayCount(event.first.weekday) - this.getWeekDayCount(this.packageDay);
+        if (dayDiff < 0) {
+            dayDiff = 7 + dayDiff;
+        }
+        //dayDiff++;
+        this.createEnabledDays(dayDiff, event.last.number, event.year, event.month);
+        switch (event.first.weekday) {
+            case 'su':
+                break;
+            case 'mo':
+                break;
+            case 'tu':
+                break;
+            case 'we':
+                break;
+            case 'th':
+                break;
+            case 'fr':
+                break;
+            case 'sa':
+                break;
+        }
     };
     //end of constructor
+    UserPackageSearchComponent.prototype.setPriceDetails = function (price, day, meridian) {
+        this.packagePrice = price;
+        this.packageDay = day;
+        this.packageMeridian = meridian;
+        //this.myDatePickerOptions.enableDays = [];
+        var d = new Date();
+        var currDay = d.getDay();
+        var selDayCount = this.getWeekDayCount(day);
+        console.log("sele" + selDayCount + "-cuur" + currDay);
+        if (selDayCount - currDay == 0) {
+            this.selDate = {
+                year: d.getFullYear(),
+                month: d.getMonth() + 1,
+                day: d.getDate()
+            };
+        }
+        else if ((currDay - selDayCount) > 0) {
+            this.selDate = {
+                year: d.getFullYear(),
+                month: d.getMonth() + 1,
+                day: d.getDate() + (7 - (currDay - selDayCount))
+            };
+        }
+        else if ((selDayCount - currDay) > 0) {
+            this.selDate = {
+                year: d.getFullYear(),
+                month: d.getMonth() + 1,
+                day: d.getDate() + (selDayCount - currDay)
+            };
+        }
+        this.preferedDate = this.selDate;
+    };
     //get packages
     UserPackageSearchComponent.prototype.getPackageByZipcode = function () {
         var _this = this;
         this.packageService.getPackageByZipcode(this.zipcode)
             .subscribe(function (data) {
             _this.availablePackages = data.result;
+            _this.getSelectedPackageDetail();
         }, function (error) {
             var body = error.json() || '';
             var err = body.error || JSON.stringify(body);
@@ -76,11 +186,29 @@ var UserPackageSearchComponent = (function () {
             alert(errr.msg);
         });
     };
+    UserPackageSearchComponent.prototype.getSelectedPackageDetail = function () {
+        if (this.selectedPackage != '') {
+            for (var i = 0; i < this.availablePackages.length; i++) {
+                if (this.availablePackages[i]._id == this.selectedPackage) {
+                    this.packageCalender = this.availablePackages[i];
+                    break;
+                }
+            }
+        }
+    };
+    UserPackageSearchComponent.prototype.setSelectedPackageDetail = function (i) {
+        this.packageCalender = this.availablePackages[i];
+    };
     UserPackageSearchComponent.prototype.cancelSelection = function () {
         this.selectedPackage = "";
         this.preferedDate = "";
         this.preferedType = "";
         this.additionalInstruction = "";
+        this.packagePriceType = "";
+        this.packagePrice = "";
+        this.packageDay = "";
+        this.packageMeridian = "";
+        this.packageCalender = {};
     };
     UserPackageSearchComponent.prototype.submitForm = function (form) {
         var _this = this;
--- client/app/components/packages/user.package.search.component.ts
+++ client/app/components/packages/user.package.search.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild, ElementRef, AfterViewInit, OnInit, OnDestroy } fr
 import { Observable } from 'rxjs/Observable';
 import { Router, RouterStateSnapshot, ActivatedRoute, Params } from '@angular/router';
 import { PackageServices, AlertService, OrderServices } from '../../services/index';
-import { IMyOptions, IMyDate, IMyDateModel } from 'mydatepicker';
+import { IMyOptions, IMyDate, IMyDateModel, IMyInputFieldChanged } from 'mydatepicker';
 
 
 import * as moment from 'moment';
@@ -24,6 +24,13 @@ export class UserPackageSearchComponent implements AfterViewInit {
   public preferedType: any = "";
   public additionalInstruction: any = "";
   private selDate: IMyDate = { year: 0, month: 0, day: 0 };
+  private packageCalender = {};
+  private packagePriceType = '';
+  private packagePrice = '';
+  private packageDay = '';
+  private packageMeridian = '';
+
+
 
   private myDatePickerOptions: IMyOptions = {
     // other options...
@@ -31,6 +38,7 @@ export class UserPackageSearchComponent implements AfterViewInit {
     editableDateField: false,
     openSelectorOnInputClick: true,
     disableUntil: { year: 0, month: 0, day: 0 }
+    //disableUntil: { year: 9999, month: 12, day: 31 }
   };
 
 
@@ -38,6 +46,86 @@ export class UserPackageSearchComponent implements AfterViewInit {
 
   onDateChanged(event: IMyDateModel) {
     // event properties are: event.date, event.jsdate, event.formatted and event.epoc
+    //console.log()
+  }
+
+  createEnabledDays(startEventNumber: number, lastEventNumber: number, eventYear: number, eventMonth: number) {
+    console.log(startEventNumber + "," + lastEventNumber + "," + eventYear + "," + eventMonth);
+    var enableDays = this.myDatePickerOptions.enableDays;
+    while (startEventNumber <= lastEventNumber) {
+      enableDays.push({ year: eventYear, month: eventMonth, day: startEventNumber });
+      startEventNumber = startEventNumber + 7;
+    }
+    //this.myDatePickerOptions.enableDays= [{ year:2017,  month:3,  day:1},{ year:2017,  month:3,  day:8}];
+  }
+
+  getWeekDayCount(weekDay: String) {
+    var weekDayNumber = 0;
+    switch (weekDay) {
+      case 'su':
+      case 'sun':
+        weekDayNumber = 1;
+        break;
+      case 'mo':
+      case 'mon':
+        weekDayNumber = 2;
+        break;
+      case 'tu':
+      case 'tue':
+        weekDayNumber = 3;
+        break;
+      case 'we':
+      case 'wed':
+        weekDayNumber = 4;
+        break;
+      case 'th':
+      case 'thur':
+        weekDayNumber = 5;
+        break;
+      case 'fr':
+      case 'fri':
+        weekDayNumber = 6;
+        break;
+      case 'sa':
+      case 'sat':
+        weekDayNumber = 7;
+        break;
+    }
+    return weekDayNumber - 1;
+  }
+  onCalendarViewChanged(event: any) {
+    console.log('onCalendarViewChanged(): Year: ', event.year, ' - month: ', event.month, ' - first: ', event.first, ' - last: ', event.last);
+
+    var dayDiff = this.getWeekDayCount(event.first.weekday) - this.getWeekDayCount(this.packageDay);
+    if (dayDiff < 0) {
+      dayDiff = 7 + dayDiff;
+    }
+    //dayDiff++;
+    this.createEnabledDays(dayDiff, event.last.number, event.year, event.month);
+
+    switch (event.first.weekday) {
+      case 'su':
+        break;
+      case 'mo':
+
+        break;
+      case 'tu':
+
+        break;
+      case 'we':
+
+        break;
+      case 'th':
+
+        break;
+      case 'fr':
+
+        break;
+      case 'sa':
+
+        break;
+    }
+
   }
 
   //constructor start
@@ -49,11 +137,11 @@ export class UserPackageSearchComponent implements AfterViewInit {
     private orderServices: OrderServices
   ) {
     let d: Date = new Date();
-    this.selDate = {
-      year: d.getFullYear(),
-      month: d.getMonth() + 1,
-      day: d.getDate()
-    };
+    // this.selDate = {
+    //   year: d.getFullYear(),
+    //   month: d.getMonth() + 1,
+    //   day: d.getDate()
+    // };
 
     this.myDatePickerOptions.disableUntil = {
       year: d.getFullYear(),
@@ -67,26 +155,65 @@ export class UserPackageSearchComponent implements AfterViewInit {
     var currentUser = JSON.parse(currentUserStr);
 
 
-    if (params.id) {
-      this.selectedPackage = params.id;
-    }
-
     if (params.zip) {
       this.zipcode = params.zip;
     } else if (currentUserStr) {
       this.zipcode = currentUser.token.zipcode;
     }
 
+
+
     this.getPackageByZipcode();
+
+    if (params.id) {
+      this.selectedPackage = params.id;
+    }
+
+    if (params.priceType) {
+      this.packagePriceType = params.priceType;
+      //this.packageCalender
+    }
 
   }
   //end of constructor
 
+  setPriceDetails(price: any, day: any, meridian: any) {
+    this.packagePrice = price;
+    this.packageDay = day;
+    this.packageMeridian = meridian;
+    //this.myDatePickerOptions.enableDays = [];
+    let d: Date = new Date();
+    var currDay = d.getDay()
+    var selDayCount = this.getWeekDayCount(day);
+    console.log("sele"+selDayCount+"-cuur"+currDay);
+    if (selDayCount-currDay == 0) {
+      this.selDate = {
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate()
+      };
+    }else if ((currDay-selDayCount) > 0 ) {
+      this.selDate = {
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate()+(7-(currDay-selDayCount))
+      };
+    }else if((selDayCount-currDay) > 0 ) {
+      this.selDate = {
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate()+(selDayCount-currDay)
+      };
+    }
+    this.preferedDate =this.selDate;
+
+  }
   //get packages
   getPackageByZipcode() {
     this.packageService.getPackageByZipcode(this.zipcode)
       .subscribe(data => {
         this.availablePackages = data.result;
+        this.getSelectedPackageDetail();
       },
       error => {
         const body = error.json() || '';
@@ -98,11 +225,33 @@ export class UserPackageSearchComponent implements AfterViewInit {
       );
   }
 
+  getSelectedPackageDetail() {
+    if (this.selectedPackage != '') {
+      for (var i = 0; i < this.availablePackages.length; i++) {
+        if (this.availablePackages[i]._id == this.selectedPackage) {
+          this.packageCalender = this.availablePackages[i];
+          break;
+        }
+      }
+    }
+  }
+
+  setSelectedPackageDetail(i: any) {
+
+    this.packageCalender = this.availablePackages[i];
+
+  }
+
   cancelSelection() {
     this.selectedPackage = "";
     this.preferedDate = "";
     this.preferedType = "";
     this.additionalInstruction = "";
+    this.packagePriceType = "";
+    this.packagePrice = "";
+    this.packageDay = "";
+    this.packageMeridian = "";
+    this.packageCalender = {};
   }
 
   submitForm(form: any): void {

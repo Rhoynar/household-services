5de2a63b95e091b5dcec14b8c877b6cc16cf519e
Shreeram R <shreeram@rhoynar.com>
2017-03-27 12:20:11 +0530

    update typescript version in package

--- client/app/models/package.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var PackageModel = (function () {
-    function PackageModel() {
-    }
-    return PackageModel;
-}());
-exports.PackageModel = PackageModel;
-//# sourceMappingURL=package.model.js.map
--- client/app/models/service.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var ServiceModel = (function () {
-    function ServiceModel() {
-    }
-    return ServiceModel;
-}());
-exports.ServiceModel = ServiceModel;
-//# sourceMappingURL=service.model.js.map
